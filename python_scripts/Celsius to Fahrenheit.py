c5199568c23d66ae13c72ba46da70e74976b2041
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2022-10-14 13:41:38 +0530

    Create Celsius to Fahrenheit.py

---
+++ python_scripts/Celsius to Fahrenheit.py
@@ -0,0 +1,10 @@
+# Input Temperature in celsius degree
+celsius = int (input("enter the temperature:"))
+
+# Converting the temperature 
+
+fahrenheit = (celsius * 1.8) + 32
+
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+	%(celsius, fahrenheit))
+
