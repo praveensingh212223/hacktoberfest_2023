e066c22bc68ab863a85360525cd2a72e628bbcd9
Tarun Gupta <tarungupta_er@rediffmail.com>
2022-10-15 20:50:34 +0530

    Create number.py (#14)

---
+++ python_scripts/number.py
@@ -0,0 +1,8 @@
+print("Enter a number:")
+number_entered = int(input())
+if(number_entered < 0):
+    print("number entered is negative")
+elif (number_entered == 0):
+    print("number entered is zero")
+else: 
+    print("number entered is positive")
