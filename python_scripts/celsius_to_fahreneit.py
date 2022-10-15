9c9a208cd6d7f5c29ba3ff2d72992a019cb5f2bc
magopoulou <77852564+magopoulou@users.noreply.github.com>
2022-10-15 03:17:07 +0300

    Create celsius_to_fahreneit.py

---
+++ python_scripts/celsius_to_fahreneit.py
@@ -0,0 +1,8 @@
+celsius = float(input("Temperature value in degree Celsius: "))
+
+# For Converting the temperature to degree Fahrenheit by using the above
+# given formula
+Fahrenheit = (celsius * 1.8) + 32
+
+# print the result
+print(f'The {celsius:.2f} degree Celsius is equal to: {Fahrenheit: .2f} Fahrenheit')
