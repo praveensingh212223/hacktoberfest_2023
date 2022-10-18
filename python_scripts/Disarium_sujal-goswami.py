4e768ea4602499e7ca17897743f368eb7f2d4d54
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2022-10-18 18:15:15 +0530

    Create Disarium_sujal-goswami.py

---
+++ python_scripts/Disarium_sujal-goswami.py
@@ -0,0 +1,29 @@
+
+import math
+
+def check(n) :
+
+	count_digits = len(str(n))
+	
+	
+	sum = 0 
+	x = n
+	while (x!=0) :
+
+		r = x % 10
+		
+		sum = (int) (sum + math.pow(r, count_digits))
+		count_digits = count_digits - 1
+		x = x//10
+		
+	if sum == n :
+		return 1
+	else :
+		return 0
+	
+n = 135
+if (check(n) == 1) :
+	print ("Disarium Number")
+else :
+	print ("Not a Disarium Number")
+
